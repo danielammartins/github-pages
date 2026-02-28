bbddf06273c41f2bad29ac1a342fe129b00bce44
Daniela Martins <martins.danielamadeira@gmail.com>
2026-02-28 23:18:28 +0000

    update cname

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <p>Hey boo, eu amo-te muito ❤️</p>
+      <p>Hey boo, eu amo-te muito </p>
     </div>
   );
 }
